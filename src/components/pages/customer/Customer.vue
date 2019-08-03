8cc3d9841bdbacb6ef38afb52ee203275ce84694
ginarizkaa <ginarizkaa@gmail.com>
2019-08-03 17:17:30 +0700

    halaman cust

---
+++ src/components/pages/customer/Customer.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="@/assets/logo.png">
+    <br>
+    halaman customer
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
